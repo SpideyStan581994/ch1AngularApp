bf1ad46abd88573f03e716c99a41564f1b438935
SpideyStan581994 <steven.tan@ulaps.dev>
2021-02-04 17:41:52 +0800

    Add Employee class
    Added some employee class mirroring the employee objects in the backend
    and add some http methods like one on the backend

--- employeemanagerapp/src/app/employee.service.ts
+++ employeemanagerapp/src/app/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,20 @@ export class EmployeeService {
 
    }
 
-   public getEmployees(): Observable<any>{
-     return this.http.get<any>('${this.apiServerUrl}/employee/all');
+   public getEmployees(): Observable<Employee[]>{
+     return this.http.get<Employee[]>('${this.apiServerUrl}/employee/all');
    }
+
+   public addEmployees(employee: Employee): Observable<Employee>{
+    return this.http.post<Employee>('${this.apiServerUrl}/employee/add',employee);
+  }
+
+  public updateEmployees(employee: Employee): Observable<Employee>{
+    return this.http.put<Employee>('${this.apiServerUrl}/employee/update',employee);
+  }
+
+  public deleteEmployees(employeeid : number): Observable<void>{
+    return this.http.delete<void>('${this.apiServerUrl}/employee/delete/${number}');
+  }
 }
+  
